e72d2109a85403ef2757ac4ca6620315abd3e7f5
Carson Clark <carsonclark@mbp14.local>
2023-12-22 16:49:30 -0800

    changed navigation color from blue to white

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Carson Clark on 2023-12-22.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    let title: String
+    var done: Bool
+}
