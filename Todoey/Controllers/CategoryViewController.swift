c68193d4ce54f8647fa999b4075a2cecd6d06913
Carson Clark <carsonclark@mbp14.local>
2024-01-05 02:00:13 -0800

    add new entity to coredata for categories to further organize todolist items

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -7,24 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    //MARK: - Tableview Datasource Methods
+    
+    //MARK: - Tableview Delegate Methods
+    
+    //MARK: - Data Manipulation Methods
+    
+    
 }
